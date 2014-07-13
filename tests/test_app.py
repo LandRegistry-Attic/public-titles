52967fb7afcb4afa6899aa8339e5106e7a5df9c8
Adam Shimali <adam.shimali@gmail.com>
2014-07-13 15:11:34 +0100

    updated tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 from publictitles import server, db
-from . factories import TitleFactory
-
+from factories import TitleFactory
+import json
 import unittest
 
 
@@ -12,11 +12,9 @@ class AppTestCase(unittest.TestCase):
         server.app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
         db.create_all()
 
-        title = TitleFactory()
+        title = TitleFactory(title_number= 'TN7654321', address = '308 Negra Arroyo Lane', postcode = '87104')
         db.session.add(title)
         db.session.commit()
-        print "This is the title"
-        print title
 
         self.app = server.app.test_client()
 
@@ -25,5 +23,9 @@ class AppTestCase(unittest.TestCase):
         db.drop_all()
 
     def test_get_title(self):
-        rv = self.app.get('/titles/TN1234567')
-        assert 'TN1234567' in rv.data
+        response = self.app.get('/titles/TN7654321')
+        response_json = json.loads(response.data)
+
+        assert response_json['title_number'] == 'TN7654321'
+        assert response_json['address'] == '308 Negra Arroyo Lane'
+        assert response_json['postcode'] == '87104'
