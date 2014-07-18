159bcd9272a9f63bb29de765ae285ca62da4625f
Adam Shimali <adam.shimali@gmail.com>
2014-07-19 00:08:50 +0100

    Put http basic auth on wrong app. Shouldn't check in late at night

--- publictitles/__init__.py
+++ publictitles/__init__.py
@@ -1,7 +1,6 @@
 import os, logging
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.basicauth import BasicAuth
 
 app = Flask(__name__)
 
@@ -14,9 +13,3 @@ if not app.debug:
 app.logger.info("\nConfiguration\n%s\n" % app.config)
 
 db = SQLAlchemy(app)
-
-if os.environ.get('BASIC_AUTH_USERNAME'):
-    app.config['BASIC_AUTH_USERNAME'] = os.environ['BASIC_AUTH_USERNAME']
-    app.config['BASIC_AUTH_PASSWORD'] = os.environ['BASIC_AUTH_PASSWORD']
-    app.config['BASIC_AUTH_FORCE'] = True
-    basic_auth = BasicAuth(app)
